737f827d47fe86fd0d53eb41f97591048a05c649
Raveh <raveh@live.cn>
2024-06-14 15:33:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "mirror-docker.raveh.net": dockerHub,
+  "mirror-quay.raveh.net": "https://quay.io",
+  "mirror-gcr.raveh.net": "https://gcr.io",
+  "mirror-k8s-gcr.raveh.net": "https://k8s.gcr.io",
+  "mirror-k8s.raveh.net": "https://registry.k8s.io",
+  "mirror-ghcr.raveh.net": "https://ghcr.io",
+  "mirror-cloudsmith.raveh.net": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.raveh.net": dockerHub,
 };
 
 function routeByHosts(host) {
